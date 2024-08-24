b731069fce7be915a1ff0d0cb5ff6ccd3c22e73f
Jay Nam <siisee111@gmail.com>
2024-08-24 13:21:41 +0900

    move(api): type

--- apps/backend/types/ingredient.d.ts
+++ apps/backend/types/ingredient.d.ts
@@ -3,16 +3,3 @@ export type Ingredient = {
   amount: string;
   optional: boolean;
 };
-
-export type CoupangProduct = {
-  name: string;
-  discount_rate: string;
-  base_price: string;
-  price: string;
-  unit_price: string;
-  arrival_info: string;
-  free_shipping: string;
-  rating_total_cnt: string;
-  reward_cash: string;
-  image: string;
-};
