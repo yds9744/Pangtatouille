df7a817ebeb3948ce1a6c990e8ed4fc945321b1b
yds9744 <yds9744@naver.com>
2024-08-24 23:07:44 +0900

    feat: enable checkbox in cart

--- apps/frontend/app/cart/page.tsx
+++ apps/frontend/app/cart/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
-import { CheckBox } from "@/components/ui/checkbox";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Product } from "@/types/product";
 import { ChevronLeft, ChevronRight, Minus, Plus } from 'lucide-react'
 
@@ -26,20 +26,29 @@ const products: Product[] = Array(20).fill(product);
 
 
 export default function Component() {
-  const [quantity, setQuantity] = useState<number[]>(Array(products.length).fill(1))
+  const [quantityList, setQuantityList] = useState<number[]>(Array(products.length).fill(1))
+  const [checkedList, setCheckedList] = useState<boolean[]>(Array(products.length).fill(true))
   const [total, setTotal] = useState(0)
 
   useEffect(() => {
     const newTotal = products.reduce((sum, product, index) => {
-      return sum + product.price * quantity[index]
+      return sum + (checkedList[index] ? product.price * quantityList[index] : 0)
     }, 0);
     setTotal(newTotal)
-  }, [quantity])
+  }, [quantityList, checkedList])
 
-  const updateQuantity = (id: number, newQuantity: number) => {
-    setQuantity((prevQuantity) =>
+  const updateQuantityList = (id: number, addNum: number) => {
+    setQuantityList((prevQuantity) =>
       prevQuantity.map((quantity, index) =>
-      index === id ? Math.max(1, newQuantity) : quantity
+        index === id ? Math.max(1, quantityList[id] + addNum) : quantity
+      )
+    );
+  }
+
+  const updateCheckedList = (id: number) => {
+    setCheckedList((prevCheckedList) =>
+      prevCheckedList.map((checked, index) =>
+        index === id ? !checked : checked
       )
     );
   }
@@ -61,7 +70,7 @@ export default function Component() {
                 <p className="text-sm text-gray-500 mb-4">
                   장바구니에는 최근에 담은 70개의 상품만 표시됩니다.
                 </p>
-                <ProductList updateQuantity={updateQuantity} quantity={quantity}/>
+                <ProductList quantityList={quantityList} updateQuantityList={updateQuantityList} updateCheckedList={updateCheckedList}/>
               </div>
             </div>
     
@@ -94,26 +103,27 @@ function CartHeader() {
   )
 }
 
-function ProductList({ updateQuantity, quantity }: {updateQuantity: (id: number, newQuantity: number) => void; quantity: number[]}) {
+function ProductList({ quantityList, updateQuantityList, updateCheckedList }: {quantityList: number[]; updateQuantityList: (id: number, addNum: number) => void; updateCheckedList: (id: number) => void}) {
   return (
     <div>
       {products.map((product, index) => (
-        <div key={product.id} className="flex items-start border-b py-4">
-        <CheckBox type="checkbox" className="mt-2 mr-4" />
+      <div key={product.id} className="flex items-center border-b py-4">
+        <Checkbox defaultChecked={true} className="mt-2 mr-4 data-[state=checked]:bg-blue-600 data-[state=checked]:border-blue-600"
+        onCheckedChange = {(e) => updateCheckedList(product.id)}/>
         <Image src={product.imageUrl} alt={product.name} width={80} height={80} className="object-cover mr-4" />
         <div className="flex-grow">
           <span className="text-sm">{product.name}</span>
           <p className="text-sm text-gray-500">{product.arrivalInfo}</p>
           <div className="flex items-center mt-2">
             <button
-              onClick={() => updateQuantity(product.id, quantity[index] - 1)}
+              onClick={() => updateQuantityList(index, -1)}
               className="border rounded p-1"
             >
               <Minus className="w-4 h-4" />
             </button>
-            <span className="mx-2">{quantity[index]}</span>
+            <span className="mx-2">{quantityList[index]}</span>
             <button
-              onClick={() => updateQuantity(product.id, quantity[index] + 1)}
+              onClick={() => updateQuantityList(product.id, 1)}
               className="border rounded p-1"
             >
               <Plus className="w-4 h-4" />
@@ -128,7 +138,7 @@ function ProductList({ updateQuantity, quantity }: {updateQuantity: (id: number,
           {product.rewardCash && (
             <p className="text-sm text-gray-500">최대 {product.rewardCash}원 적립</p>
           )}
-          <span className="text-sm text-blue-500">로켓프레시</span>
+          <Image src="https://image6.coupangcdn.com/image/badges/falcon/v1/web/rocket-fresh@2x.png" alt="로켓프레시" width={63} height={14}/>
         </div>
       </div>
       ))}
@@ -160,8 +170,8 @@ function TotalPrice({total} : {total: number}) {
             <span>{total.toLocaleString()}원</span>
           </div>
         </div>
-        <button className="w-full bg-blue-500 text-white py-3 rounded-lg mt-6 font-bold">
-          구매하기 (0)
+        <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg mt-6 font-bold">
+          구매하기
         </button>
       </div>
     </div>
--- apps/frontend/app/products/page.tsx
+++ apps/frontend/app/products/page.tsx
@@ -97,7 +97,7 @@ export default function Component() {
               <div className="flex items-center gap-2 mb-4">
                 <Input type="number" value={1} min={1} className="w-20" />
                 <Button variant="outline" className="flex-grow border-blue-600 text-blue-600">장바구니 담기</Button>
-                <Button className="flex-grow bg-blue-600 text-white font-bold">바로구매 &gt;</Button>
+                <Button className="flex-grow bg-blue-600 text-white font-bold hover:bg-blue-700">바로구매 &gt;</Button>
               </div>
 
             </div>
