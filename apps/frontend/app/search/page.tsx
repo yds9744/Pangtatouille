f96e7971a9d6aa213674c427ba75f9768514e4be
Jay Nam <siisee111@gmail.com>
2024-08-25 00:37:47 +0900

    feat(web): populate mock products

--- apps/frontend/app/search/page.tsx
+++ apps/frontend/app/search/page.tsx
@@ -5,6 +5,7 @@ import { Product } from "@/types/product";
 import { Suspense } from "react";
 import { RecipeCards } from "@/app/search/components/recipe-cards";
 import { RecipeCardSkeleton } from "@/app/search/components/recipe-card-skeleton";
+import { mockProducts } from "@/app/search/constant/product.constant";
 
 export default async function Search({
   params,
@@ -44,24 +45,7 @@ export default async function Search({
 }
 
 function ProductList({ keyword }: { keyword: string }) {
-  const product: Product = {
-    id: 0,
-    name: "흰다리 새우살 (냉동), 300g(26~30size), 1팩",
-    discountRate: 21,
-    basePrice: 9900,
-    price: 7730,
-    amount: 1,
-    unitPriceText: "(100g당 2,577원)",
-    arrivalInfo: "내일(토) 새벽 도착 보장",
-    ratingTotalCnt: 3058,
-    rewardCash: 77,
-    imageUrl:
-      "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1200073317916374-985075ca-74a7-45f5-b956-fd65088e99a7.jpg",
-    amountUnit: "g",
-    quantity: 1,
-    quantityUnit: "팩",
-  };
-  const products: Product[] = Array(20).fill(product);
+  const products: Product[] = mockProducts;
 
   return (
     <div className="grid grid-cols-4 gap-4">
