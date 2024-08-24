c87517cf87592eb8b74c31e133c9553c69b52ce2
Jay Nam <siisee111@gmail.com>
2024-08-24 14:13:50 +0900

    feat(api): add Post /recipe

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -2,11 +2,19 @@ import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ingredient } from 'types/ingredient';
 import { GetIngredientsDto } from 'src/app.dto';
+import { Recipe } from 'types/recipe';
 
 @Controller()
 export class AppController {
   protected readonly logger = new Logger(this.constructor.name);
   constructor(private readonly appService: AppService) {}
+
+  @Post('recipe')
+  async getRecipe(@Body() body: GetIngredientsDto): Promise<Recipe> {
+    this.logger.log('Getting recipe...');
+    body.description = GetIngredientsDto.mock().description;
+    return await this.appService.getRecipe(body.description);
+  }
 
   @Post('ingredients')
   async getIngredients(@Body() body: GetIngredientsDto): Promise<Ingredient[]> {
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { OpenAIService } from 'libs/openai/openai.service';
 import { Ingredient } from 'types/ingredient';
+import { Recipe } from 'types/recipe';
 
 @Injectable()
 export class AppService {
   constructor(private readonly openAIService: OpenAIService) {}
+
+  async getRecipe(description: string): Promise<Recipe> {
+    return await this.openAIService.extractRecipe(description);
+  }
 
   async getIngredients(description: string): Promise<Ingredient[]> {
     return await this.openAIService.extractIngredients(description);
