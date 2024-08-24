bf7fe838f81251ef51b537238ffe9d11dc144cc9
HTaeha <xogk39@gmail.com>
2024-08-24 17:46:43 +0900

    Modify parsing code

--- apps/parsing/parse.py
+++ apps/parsing/parse.py
@@ -11,20 +11,21 @@ def split_quantity_unit(input_string):
         unit = match.group(2)  # 단위 부분
         return quantity, unit
     else:
-        raise ValueError("입력 문자열에서 숫자와 단위를 찾을 수 없습니다.")
+        return None, None
 
 directory = 'data/input'
 for dirpath, _, filenames in os.walk(directory):
-    for filename in filenames:
-        with open('data/input/'+filename, 'r') as input_file:
-            with open('data/output/'+filename, 'a') as output_file:
-                reader = csv.reader(input_file, delimiter=',')
-                writer = csv.writer(output_file)
-                writer.writerow(next(reader) + ["quantity", "unit"])
-                for row in reader:
-                    name = row[0]
-                    split_text = name.split(', ')
-                    if len(split_text) > 1:
-                        quantity_unit = split_text[1]
-                        quantity, unit = split_quantity_unit(quantity_unit)
-                        writer.writerow(row + [quantity, unit])
+    with open('data/output/result.csv', 'a') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(["name", "discount_rate", "base_price", "price", "unit_price_text", "arrival_info", "rating_total_cnt", "reward_cash", "image_url", "quantity", "unit"])
+        for filename in filenames:
+            with open('data/input/'+filename, 'r') as input_file:
+                    reader = csv.reader(input_file, delimiter=',')
+                    for row in reader:
+                        name = row[0]
+                        split_text = name.split(', ')
+                        if len(split_text) > 1:
+                            quantity_unit = split_text[1]
+                            quantity, unit = split_quantity_unit(quantity_unit)
+                            if quantity is not None:
+                                writer.writerow(row + [quantity, unit])
