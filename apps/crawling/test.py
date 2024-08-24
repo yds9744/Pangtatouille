7827b9431f069b2bd8b4a772842c03edcf910768
taejuk <taeju456@gmail.com>
2024-08-24 16:48:18 +0900

    크롤링

--- apps/crawling/test.py
+++ apps/crawling/test.py
@@ -6,80 +6,69 @@ import json
 import csv
 
 
-url_init = 'https://www.coupang.com/np/search?component=&q='
-
-keyword = "닭가슴살"
-url = url_init+keyword
-url = parse.urlparse(url)
-query = parse.parse_qs(url.query)
-result = parse.urlencode(query, doseq=True)
-result = result[2:]
-url = url_init + result + "&channel=user"
+# category를 다 가져오자
+init_url = "https://www.coupang.com/np/categories/464568"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36", "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3"}
-res = requests.get(url, headers=headers)
+res = requests.get(init_url, headers=headers)
 html = res.text
 soup = BeautifulSoup(html, "html.parser")
-items = soup.select("[class=search-product]")
-
-# print(items[0])
-# productid ㅊ
-
+title = soup.select_one(".newcx-product-list-title").get_text(strip=True)
+title = "국내산_" + title.replace("/","_")
+items = soup.select("li.baby-product")
 itemList = []
+# 먼저 category id를 쭉 가져와야 한다.
+
 for item in items:
-    data =  json.loads(item.select_one("[class=search-product-link]").attrs.get('data-srp-log'))
-    productId = data["productId"]
-    name = item.select_one(".name").text
-    dc_percent = ""
-    if(item.select_one(".instant-discount-rate")):
-        dc_percent = item.select_one(".instant-discount-rate").text
-    
+    # productId를 가져온다.
+    productId = item.attrs.get("id")
+    image = "https:"+str(item.select_one("img").attrs.get("src"))
+    descriptions = item.select_one(".descriptions")
+    name = descriptions.select_one(".name").get_text(strip=True)
+    discount_rate = ""
+    base_price = ""
     price = ""
-    if(item.select_one(".base-price")):
-        price = item.select_one(".base-price").text
-    dc_price = item.select_one(".price-value").text
-    unit_price = ""
-    delivery_time = ""
-    review = ""
-    reward_text = ""
-    point = ""
-    delivery_price = ""
+    unit_price_text = ""
+    arrival_info = ""
+    free_shopping = ""
+    rating_total_cnt = ""
+    reward_cash = ""
     
-    if(item.select_one(".unit-price")):
-        unit_price = item.select_one(".unit-price").text
-    if(item.select_one(".arrival-info")):
-        delivery_time = item.select_one(".arrival-info").text
-    if(item.select_one(".rating-total-count")):
-        review = item.select_one(".rating-total-count").text
-    if(item.select_one(".reward-cash-txt")):
-        reward_text = item.select_one(".reward-cash-txt").text
-    if(reward_text != ""):
-        reward = reward_text[3:-4]
-    if(item.select_one('.badge-delivery')):
-        delivery_price = item.select_one('.badge-delivery').text
-    image = ""
-    if(item.select_one(".search-product-wrap-img").attrs.get('data-img-src')):
-        image = "https:" + str(item.select_one(".search-product-wrap-img").attrs.get('data-img-src'))
-    else:
-        image = "https:" + str(item.select_one(".search-product-wrap-img").attrs.get('src'))
+    if(descriptions.select_one(".discount-percentage")):
+        discount_rate = descriptions.select_one(".discount-percentage").get_text(strip=True)[:-1]
+    if(descriptions.select_one(".base-price")):
+        base_price = descriptions.select_one(".base-price").get_text(strip=True)
+    if(descriptions.select_one(".price-value")):
+        price = descriptions.select_one(".price-value").get_text(strip=True)
+    if(descriptions.select_one(".unit-price")):
+        unit_price_text = descriptions.select_one(".unit-price").get_text(strip=True)
+    if(descriptions.select_one(".arrival-info")):
+        arrival_info = descriptions.select_one(".arrival-info").get_text(strip=True)
+    if(descriptions.select_one(".rating-total-count")):
+        rating_total_cnt = descriptions.select_one(".rating-total-count").get_text(strip=True)[1:-1]
+    if(descriptions.select_one(".reward-cash-txt")):
+        reward_cash = descriptions.select_one(".reward-cash-txt").get_text(strip=True)[3:-4]
+    price = price.replace(",","")
+    base_price = base_price.replace(",","")
+    rating_total_cnt = rating_total_cnt.replace(",","")
+    reward_cash = reward_cash.replace(",","")
+    itemDic = [
+    name,
+    discount_rate,
+    base_price,
+    price,
+    unit_price_text,
+    arrival_info,
+    rating_total_cnt,
+    reward_cash,
+    image,
+    ]
     
-    item_dic = {
-        "productId": productId,
-        "name": name,
-        "dc_percent": dc_percent,
-        "price": price,
-        "dc_price": dc_price,
-        "unit_price": unit_price,
-        "delivery_time": delivery_time,
-        "delivery_price": delivery_price,
-        "review":review,
-        "point":point,
-        "image":image
-    }
-    itemList.append(item_dic)
-
-
-filename = keyword+".csv"
-
-with open(filename, "w") as file:
+    
+    itemList.append(itemDic)
+with open(title + ".csv", "w") as file:
     writer = csv.writer(file)
-    writer.writerow(itemList)
+    kind = ["name","discount_rate","base_price","price", "unit_price_text","arrival_info","rating_total_cnt","reward_cash","image_url"]
+    writer.writerow(kind)
+    for i in itemList:
+        writer.writerow(i)
+   
