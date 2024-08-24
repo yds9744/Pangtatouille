1d510bd8e86f2c9692e69d9c5ffbec688c8a6997
Jay Nam <siisee111@gmail.com>
2024-08-25 03:23:43 +0900

    feat: no cache

--- apps/frontend/app/search/components/recipe-cards.tsx
+++ apps/frontend/app/search/components/recipe-cards.tsx
@@ -3,7 +3,8 @@ import { RecipeCard } from "@/app/search/components/recipe-card";
 
 export async function RecipeCards({ keyword }: { keyword: string }) {
   const productPackages: ProductPackage[] = await fetch(
-    `${process.env.SERVER_BASE_URL}/search/product-package/youtube?query=${keyword}`
+    `${process.env.SERVER_BASE_URL}/search/product-package/youtube?query=${keyword}`,
+    { cache: "no-cache" }
   ).then((res) => res.json());
 
   return (
