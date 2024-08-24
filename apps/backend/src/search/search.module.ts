2e5802988d705980c533f2ca360b374228332069
HTaeha <xogk39@gmail.com>
2024-08-25 01:53:17 +0900

    product package 저장

--- apps/backend/src/search/search.module.ts
+++ apps/backend/src/search/search.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
+import { ProductPackageService } from 'src/product-package/product-package.service';
 
 @Module({
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, ProductPackageService],
 })
 export class SearchModule {}
--- apps/backend/src/search/search.service.ts
+++ apps/backend/src/search/search.service.ts
@@ -1,16 +1,61 @@
 import { Product } from '@lib/database/product/product.entity';
 import { Injectable, Logger } from '@nestjs/common';
+import { OpenAIService } from 'libs/openai/openai.service';
 import { searchRecipeVideo } from 'libs/search/youtube';
+import { ProductPackageService } from 'src/product-package/product-package.service';
 import { Ingredient } from 'types/ingredient';
 import { Video } from 'types/video';
 
 @Injectable()
 export class SearchService {
+  constructor(
+    private readonly openaiService: OpenAIService,
+    private readonly productPackageService: ProductPackageService,
+  ) {}
   protected readonly logger = new Logger(this.constructor.name);
 
   async searchRecipeVideoOnYoutube(query: string): Promise<Video[]> {
     const videos: Video[] = await searchRecipeVideo(query);
     return videos;
+  }
+
+  async getProductPackage(query: string) {
+    const videos = await this.searchRecipeVideoOnYoutube(query);
+    const recipeVideos = (
+      await Promise.all(
+        videos.map(async (video) => {
+          const isRecipe = await this.openaiService.isRecipe(video.description);
+          return { ...video, isRecipe };
+        }),
+      )
+    ).filter((video) => video.isRecipe);
+
+    if (recipeVideos.length > 0) {
+      const productPackages = await Promise.all(
+        recipeVideos.map(async (video) => {
+          const recipeAndIngredients =
+            await this.openaiService.extractRecipeAndIngredients(
+              video.description,
+            );
+          const products = await this.searchByIngredients(
+            recipeAndIngredients.ingredients,
+          );
+          const productPackage = await this.productPackageService.createOne(
+            video,
+            recipeAndIngredients.ingredients,
+            recipeAndIngredients.recipe,
+            products,
+          );
+          return productPackage;
+        }),
+      );
+      this.logger.log(
+        `productPackages: ${JSON.stringify(productPackages, null, 2)}`,
+      );
+      return productPackages;
+    }
+
+    return [];
   }
 
   async search(keyword: string): Promise<Product[]> {
