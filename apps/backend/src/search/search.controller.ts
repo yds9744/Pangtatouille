62d6c31ecaa0197a38b87412461cd5c454fdb59d
Jay Nam <siisee111@gmail.com>
2024-08-24 13:32:37 +0900

    feat(api): add youtube search

--- apps/backend/src/search/search.controller.ts
+++ apps/backend/src/search/search.controller.ts
@@ -1,25 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { OpenAIService } from '../../libs/openai/openai.service';
 
-@Controller('system')
+@Controller('search')
 export class SearchController {
   constructor(
     private readonly systemService: SearchService,
     private readonly openaiService: OpenAIService,
   ) {}
 
-  @Get('health')
-  health(): string {
-    return this.systemService.health();
-  }
-
-  @Get('health/openai')
-  openaiHealth(): string {
-    if (this.openaiService.openai) {
-      return 'OpenAI is healthy';
-    } else {
-      return 'OpenAI is not healthy';
-    }
+  @Get()
+  search(@Query('query') query: string): Promise<string> {
+    return this.systemService.searchYoutube(query);
   }
 }
--- apps/backend/src/search/search.service.ts
+++ apps/backend/src/search/search.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
+import * as yt from 'youtube-search-without-api-key';
 
 @Injectable()
 export class SearchService {
   protected readonly logger = new Logger(this.constructor.name);
 
-  health(): string {
-    this.logger.log('Healthy!');
+  async searchYoutube(query: string): Promise<string> {
+    const videos = await yt.search(query);
+    this.logger.log(`Found videos: ${JSON.stringify(videos, null, 2)}`);
     return 'Healthy!';
   }
 }
