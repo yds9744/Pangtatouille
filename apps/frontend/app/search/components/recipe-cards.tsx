cab92828eea3b1b4712b02417f9ac2ef1064ac1c
Jay Nam <siisee111@gmail.com>
2024-08-24 23:26:57 +0900

    feat: example recipe page

--- apps/frontend/app/search/components/recipe-cards.tsx
+++ apps/frontend/app/search/components/recipe-cards.tsx
@@ -3,8 +3,7 @@ import { RecipeCard } from "@/app/search/components/recipe-card";
 
 export async function RecipeCards({ keyword }: { keyword: string }) {
   const fullRecipes: FullRecipe[] = await fetch(
-    `http://localhost:8000/search/full-recipe/youtube/mock?query=${keyword}`,
-    { cache: "no-store" }
+    `http://localhost:8000/search/full-recipe/youtube/mock?query=${keyword}`
   ).then((res) => res.json());
 
   return (
